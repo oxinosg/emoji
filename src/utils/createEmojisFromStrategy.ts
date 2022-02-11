2d6d55d183d2f4d966657c0cd56f01901eb6be3c
oxinosg <georgios.oxinos.extern@elinvar.de>
2022-02-11 14:50:21 +0100

    first commit

---
+++ src/utils/createEmojisFromStrategy.ts
@@ -0,0 +1,39 @@
+import { emojiList, toShort } from 'emoji-toolkit';
+import data from 'emojibase-data/en/compact.json';
+
+export interface EmojiStrategy {
+  [x: string]: {
+    [x: string]: string[];
+  };
+}
+
+// Filtering out all non printable characters.
+// All the printable characters of ASCII are conveniently in one continuous range
+function escapeNonASCIICharacters(str: string): string {
+  return str.replace(/[^ -~]+/g, '');
+}
+
+export default function createEmojisFromStrategy(): EmojiStrategy {
+  const emojis: EmojiStrategy = {};
+
+  for (const item of data) {
+    const shortName = toShort(item.unicode);
+    const emoji = emojiList[escapeNonASCIICharacters(shortName)];
+    if (emoji) {
+      if (!emojis[emoji.category]) {
+        emojis[emoji.category] = {};
+      }
+      emojis[emoji.category][shortName] = [shortName];
+
+      if (item.skins) {
+        for (const skin of item.skins) {
+          const skinShortName = toShort(skin.unicode);
+          if (emojiList[skinShortName]) {
+            emojis[emoji.category][shortName].push(skinShortName);
+          }
+        }
+      }
+    }
+  }
+  return emojis;
+}
